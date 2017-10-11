15dfd0dc66924c5ad55a54eaf643c7e2f002d0cc
derek <derek@llamabagel.ca>
2017-10-11 18:08:11 -0400

    Included GPL3 notices in source files

--- PrestoApi/Startup.cs
+++ PrestoApi/Startup.cs
@@ -1,7 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿/**
+ *  This file is part of Llamabagel's Presto Api.
+ *
+ *  Llamabagel's Presto Api is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Llamabagel's Presto Api is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Llamabagel's Presto Api.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
