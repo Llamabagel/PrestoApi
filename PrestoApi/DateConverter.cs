252845c8621711859a577519ab62745ce9c00c28
derek <derek@llamabagel.ca>
2017-10-11 18:08:11 -0400

    Included GPL3 notices in source files

--- PrestoApi/DateConverter.cs
+++ PrestoApi/DateConverter.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/**
+ *  This file is part of Llamabagel's Presto Api.
+ *
+ *  Llamabagel's Presto Api is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Llamabagel's Presto Api is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Llamabagel's Presto Api.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
